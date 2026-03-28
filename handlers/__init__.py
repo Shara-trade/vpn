0160ed1ffc99a43baf532bb01acc077869252caa
Shara-trade <dengalkin524@gmail.com>
2026-03-28 21:54:52 +0300

    Add files via upload

---
+++ handlers/__init__.py
@@ -0,0 +1,17 @@
+"""
+Модуль обработчиков StarinaVPN.
+"""
+
+from handlers import start, menu, profile, keys, purchase, callbacks, admin, topup, promocode
+
+__all__ = [
+    "start",
+    "menu", 
+    "profile",
+    "keys",
+    "purchase",
+    "callbacks",
+    "admin",
+    "topup",
+    "promocode",
+]
