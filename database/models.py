4137630e98ba19471eb6e0adb3b775b134dc7583
Shara-trade <dengalkin524@gmail.com>
2026-03-31 18:50:28 +0300

    models.py

--- database/models.py
+++ database/models.py
@@ -71,7 +71,7 @@ async def init_db() -> None:
             inbound_id INTEGER NOT NULL DEFAULT 1,
             is_active BOOLEAN NOT NULL DEFAULT 1,
             is_trial BOOLEAN NOT NULL DEFAULT 0,
-            capacity INTEGER NOT NULL DEFAULT 0,
+            capacity INTEGER NOT NULL DEFAULT 200,
             load INTEGER NOT NULL DEFAULT 0,
             ping INTEGER NOT NULL DEFAULT 0,
             created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
